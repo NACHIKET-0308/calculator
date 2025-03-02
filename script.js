ce1f92cf3bced189607ff889507b03388609718f
NACHIKET MISHRA <nachiket22m@gmail.com>
2025-03-02 23:08:09 +0530

    basic changes

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function init(){
-
+console.log("hi")
+document
+.querySelector(".calc-buttons")
+.addEventListener("click",function(event){
+    buttonClick(event.target.innerText);
+});
 }
 init();
