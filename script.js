8631e19185f3d6262607ed9716abc72c91176ff6
NACHIKET MISHRA <nachiket22m@gmail.com>
2025-03-03 22:50:41 +0530

    basic changes

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+function buttonClick(value){
+    if (isNaN(parseInt(value))){
+        handleSymbol(value);
+    }else {
+        handleNumber(value);
+    }
+}
+function handleSymbol(value) {
+    console.log("symbol");
+}
+function handleNumber(value){
+    console.log("number");
+}
+
+
+
 function init(){
 console.log("hi")
 document
 .querySelector(".calc-buttons")
 .addEventListener("click",function(event){
     buttonClick(event.target.innerText);
+    
 });
 }
 init();
